a6b78321353479d47fb61f5923451c2ac7c0473d
rosecondon <35875000+rosecondon@users.noreply.github.com>
2020-04-22 09:16:05 -0400

    init check in

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3008;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Testing Docker Image with Node js!\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
